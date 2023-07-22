6a52883380e13ab6e20df5810e9b99e805270748
Maurya2811 <maurya2811@gmail.com>
2023-07-22 15:15:23 +0530

    secondcomment

--- task2/src/App.js
+++ task2/src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 
 
-function App() {
+function App(){
   const [trainSchedule, setTrainSchedule] = useState([]);
   useEffect(() => {
     //fetchTrainSchedule();
@@ -21,45 +21,45 @@ function App() {
   //   // Handle errors
   // });
   const apiUrl = 'http://20.244.56.144/train/trains'; // Replace this with the actual API URL
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTAwMTEzNzksImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiODQyMTE5YTYtODRlNC00YTg1LWI5ODctM2U5MDE4MmM0MmMwIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IjIwMDAzMzAxNDQifQ.hZbHgw8ffyg_BX3axn4h4qFZq7_aiu7duf-T3WWPcRQ'; // Replace this with the actual token
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTAwMTEzNzksImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiODQyMTE5YTYtODRlNC00YTg1LWI5ODctM2U5MDE4MmM0MmMwIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IjIwMDAzMzAxNDQifQ.hZbHgw8ffyg_BX3axn4h4qFZq7_aiu7duf-T3WWPcRQ'; // Replace this with the actual token
 
-  const requestOptions = {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`
+const requestOptions = {
+  method: 'GET',
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+};
+
+let train =[];
+
+fetch(apiUrl, requestOptions)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
-  };
-
-  let train = [];
-
-  fetch(apiUrl, requestOptions)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      // Do something with the data
-      setTrainSchedule(data);
-      console.log(data);
-    })
-    .catch(error => {
-      // Handle errors
-      console.error('Fetch error:', error);
-    });
+    return response.json();
+  })
+  .then(data => {
+    // Do something with the data
+   setTrainSchedule(data);
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle errors
+    console.error('Fetch error:', error);
+  });
 
 
   const fetchTrainSchedule = async () => {
     try {
-      const response = await axios.get("http://20.244.56.144/train/trains/?token=" + token);
+      const response = await axios.get("http://20.244.56.144/train/trains/?token="+token);
       console.log(response.data);
       setTrainSchedule(response.data);
     } catch (error) {
       console.error('Error fetching train schedule:', error);
     }
   };
-
+  
 
   return (
     <div className="App">
@@ -76,11 +76,11 @@ function App() {
               <th>Destination</th>
             </tr>
           </thead>
-          <tbody>
+           <tbody>
 
-            {trainSchedule.map((array, index) => {
+            {trainSchedule.map((array,index)=>{
               <div key={index}>
-
+                
               </div>
             })}
           </tbody>
