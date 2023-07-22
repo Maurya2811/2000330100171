7c634ea2913fa6ea574a1366f36852dcc1823ad6
Maurya2811 <maurya2811@gmail.com>
2023-07-22 15:03:02 +0530

    first

--- task1/src/index.js
+++ task1/src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require("express");
+const axios = require("axios");
+const async = require("async");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = express();
+const port = 8008;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.get("/numbers", async (req, res) => {
+  const urls = req.query.url;
+
+  if (!urls || !Array.isArray(urls)) {
+    return res
+      .status(400)
+      .json({ error: 'Invalid or missing "url" parameter.' });
+  }
+
+  const validUrls = urls.filter((url) => isValidUrl(url));
+
+  if (validUrls.length === 0) {
+    return res.status(400).json({ error: "No valid URLs provided." });
+  }
+
+  try {
+    const numbersArray = await fetchNumbersFromUrls(validUrls);
+    const mergedNumbers = mergeAndSortNumbers(numbersArray);
+    return res.json({ numbers: mergedNumbers });
+  } catch (error) {
+    console.error("Error processing the request:", error);
+    return res.status(500).json({ error: "Error processing the request." });
+  }
+});
+
+function isValidUrl(url) {
+
+  return true;
+}
+
+async function fetchNumbersFromUrls(urls) {
+  const promises = urls.map((url) => {
+    return axios
+      .get(url)
+      .then((response) => response.data.numbers)
+      .catch(() => []); 
+  });
+
+  const session = 500;
+  const options = { session };
+
+  return await async.session(async.parallel(promises), session, options);
+}
+
+function mergeAndSortNumbers(numbersArray) {
+  const mergedSet = new Set();
+  numbersArray.forEach((numbers) => {
+    numbers.forEach((number) => mergedSet.add(number));
+  });
+
+  return Array.from(mergedSet).sort((a, b) => a - b);
+}
+
+app.listen(port, () => {
+  console.log(
+    `Number Management Service listening at http://localhost:${port}`
+  );
+});
